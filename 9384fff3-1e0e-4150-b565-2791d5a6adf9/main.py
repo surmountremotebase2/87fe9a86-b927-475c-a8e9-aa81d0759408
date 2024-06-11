ae31512104b4304717a95cc18c9b8c7cc9a88d09
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-11 06:21:56 -0400

    User Hassan Raza saved changes to main.py at 06/11/2024, 10:21:55 UTC time.

--- 9384fff3-1e0e-4150-b565-2791d5a6adf9/main.py
+++ 9384fff3-1e0e-4150-b565-2791d5a6adf9/main.py
@@ -1,1 +1,16 @@
 #Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+
+   def run(self, data):
+      qqq_stake = 0
+      if len(d)>3 and "13:00" in d[-1]["QQQ"]["date"]:
+         v_shape = d[-2]["QQQ"]["close"]<d[-3]["QQQ"]["close"] and d[-1]["QQQ"]["close"]>d[-2]["QQQ"]["close"]
+         log(str(v_shape))
+         if v_shape:
+            qqq_stake = 1
+
+      return TargetAllocation({"QQQ": qqq_stake})
